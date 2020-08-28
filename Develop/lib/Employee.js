af70511590b5523554291e5bfa6dc0a02aa82140
Alissa Champigny <achampigny4@gmail.com>
2020-08-28 13:19:40 -0400

    test suite passed all 8 tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,22 +1,22 @@
 // TODO: Write code to define and export the Employee class
 function Employee(name, id, email) {
-    this.id = id;
     this.name = name;
+    this.id = id;
     this.email = email;
     this.role = "Employee";
 
     Employee.prototype.getName = function(name){
         return this.name;
-    // };
-    // getId() {
-    //     return this.id;
-    // };
-    // getEmail() {
-    //     return this.email;
-    // };
-    // getRole() {
-    //     return this.role;
-    }; // Returns 'Employee'
+    };
+    Employee.prototype.getId = function(id){
+        return this.id;
+    };
+    Employee.prototype.getEmail = function(email){
+        return this.email;
+    };
+    Employee.prototype.getRole = function(role){
+        return this.role;
+    };
 };
 
 module.exports = Employee;
