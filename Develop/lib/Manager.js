8678ebfc9d5e35f96d2153f94c282a67d1b5e1b7
Alissa Champigny <achampigny4@gmail.com>
2020-08-28 13:30:11 -0400

    manager test suite passed

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+function Manager(name, id, email, officeNumber) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.officeNumber = officeNumber;
+    this.role = "Manager";
+
+    Manager.prototype.getName = function (name) {
+        return this.name;
+    };
+    Manager.prototype.getId = function (id) {
+        return this.id;
+    };
+    Manager.prototype.getEmail = function (email) {
+        return this.email;
+    };
+    Manager.prototype.getOffice = function (officeNumber) {
+        return this.officeNumber;
+    };
+    Manager.prototype.getRole = function (role) {
+        return this.role;
+    };
+};
+
+module.exports = Manager;
