6be7190829b001fd9d3b9f93f0b86595a58ca689
Alissa Champigny <achampigny4@gmail.com>
2020-08-28 13:09:34 -0400

    5testPass:name,id, email,getName, EmployeeInstance

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+function Employee(name, id, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.role = "Employee";
+
+    Employee.prototype.getName = function(name){
+        return this.name;
+    // };
+    // getId() {
+    //     return this.id;
+    // };
+    // getEmail() {
+    //     return this.email;
+    // };
+    // getRole() {
+    //     return this.role;
+    }; // Returns 'Employee'
+};
+
+module.exports = Employee;
