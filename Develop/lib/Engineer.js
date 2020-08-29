196e3992c114d75438baf7626dd4050858e4845f
Alissa Champigny <achampigny4@gmail.com>
2020-08-29 14:22:34 -0400

    2test pass: set Github, getRole() return engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,25 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-function Engineer(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
         this.role = "Engineer";
-        Engineer.prototype.getName = function(name){
-            return this.name;
-        };
-        Engineer.prototype.getId = function(id){
-            return this.id;
-        };
-        Engineer.prototype.getEmail = function(email){
-            return this.email;
-        };
-        Engineer.prototype.getRole = function(role){
-            return this.role;
-        };
+        this.github = github;
     };
 
+};
+
 module.exports = Engineer;
-//test results
-// × Can set GitHUb account via constructor (5 ms)
+// √ Can set GitHUb account via constructor (1 ms)
 // √ getRole() should return "Engineer"
 // × Can get GitHub username via getGithub() (1 ms)
