6fdbaa58fa65e6e3ff1761b2150e87722555dd14
Alissa Champigny <achampigny4@gmail.com>
2020-08-30 20:06:18 -0400

    consolidated code for functions

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,24 +1,26 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    this.role = "Employee";
-    //https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
-    Employee.prototype.getName = function(name){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = "Employee";
+    }
+    getName() {
         return this.name;
-    };
-    Employee.prototype.getId = function(id){
+    }
+
+    getId() {
         return this.id;
-    };
-    Employee.prototype.getEmail = function(email){
+    }
+
+    getEmail() {
         return this.email;
-    };
-    Employee.prototype.getRole = function(role){
+    }
+
+    getRole() {
         return this.role;
-    };
-};
+    }
 };
 
 module.exports = Employee;
